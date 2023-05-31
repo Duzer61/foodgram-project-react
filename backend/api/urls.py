893c986ce9428553acbec2ce6545d7c681684b1d
Данил Кочетов <dmi194@yandex.ru>
2023-05-31 17:12:16 +0300

    инициировал приложение api

---
+++ backend/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import include, path
+
+app_name = 'api'
+
+
+urlpatterns = [
+    # path('v1/', include(router.urls)),
+    # path('', include('djoser.urls')),
+    # path('', include('djoser.urls.jwt')),
+]
