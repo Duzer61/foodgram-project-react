59709a6badb2fc8b4353037771fa3675ce081990
Данил Кочетов <dmi194@yandex.ru>
2023-06-10 16:48:11 +0300

    Поменял пагинацию по умолчанию

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -72,8 +72,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
         # 'rest_framework.permissions.AllowAny',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 6,
+    'DEFAULT_PAGINATION_CLASS': 'api.paginators.CustomPagination',
 }
 
 DJOSER = {
