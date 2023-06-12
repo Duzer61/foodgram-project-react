3cf41d84e429c57a35c321c965597d40810da98a
Данил Кочетов <dmi194@yandex.ru>
2023-06-12 20:09:32 +0300

    Добавил фильт на избранные в рецептах

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,17 +1,23 @@
 from django_filters import rest_framework
 
-from .views import Recipe, Tag
+from .views import Recipe, Tag, User
 
 
 class RecipeFilter(rest_framework.FilterSet):
-    author = rest_framework.NumberFilter(field_name='author')
+    author = rest_framework.ModelChoiceFilter(queryset=User.objects.all())
     tags = rest_framework.ModelMultipleChoiceFilter(
         field_name='tags__slug',
         to_field_name='slug',
         queryset=Tag.objects.all()
     )
-    #is_favorited = rest_framework.BooleanFilter(field_name='is_favorited')
+    is_favorited = rest_framework.BooleanFilter(method='is_favorited_method')
+
+    def is_favorited_method(self, queryset, name, value):
+        if value:
+            queryset = queryset.filter(favourite__user=self.request.user)
+        return queryset
+        
 
     class Meta:
         model = Recipe
-        fields = ['author', 'tags', 'is_favorited']
+        fields = ['author', 'tags']
